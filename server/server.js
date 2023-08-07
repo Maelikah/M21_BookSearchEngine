26283380990fd3c7f623b933805b1751ef6cd4f2
Maelikah <eva.longoria@gmail.com>
2023-08-07 12:09:26 -0600

    fixed code in auth.js because it was not allowing to sign up or login

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // Require apollo server and add schemas
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require("./utils/auth");
+const { authMiddleware } = require('./utils/auth');
 
 const path = require('path');
 const db = require('./config/connection');
@@ -16,6 +16,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+  introspection: true,
 });
 
 app.use(express.urlencoded({ extended: true }));
